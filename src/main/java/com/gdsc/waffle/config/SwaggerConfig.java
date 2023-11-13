d3033f0b696a03da5c5ee549c6162577ec9cb3e8
Seung-9 <ssg9505fj22@naver.com>
2023-11-13 21:08:51 +0900

    chore: 테스트 코드 제거

--- src/main/java/com/gdsc/waffle/config/SwaggerConfig.java
+++ src/main/java/com/gdsc/waffle/config/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package com.gdsc.waffle.config;
 
-import com.gdsc.waffle.TestController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
