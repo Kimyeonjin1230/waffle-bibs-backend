00bbc09f3a9045c6772c743cbaade49e3038dde1
Seung-9 <ssg9505fj22@naver.com>
2023-11-13 13:31:46 +0900

    chore: test 코드 제거

--- src/main/java/com/gdsc/waffle/config/SwaggerConfig.java
+++ src/main/java/com/gdsc/waffle/config/SwaggerConfig.java
@@ -30,8 +30,6 @@ public class SwaggerConfig {
                 .apiInfo(apiInfo())
                 // swagger에서 제공하는 기본 응답 코드 설명 제거
                 .useDefaultResponseMessages(false)
-                // API문서에서 제외할 class 지정. "," 로 이어서 작성해도 됨.
-                .ignoredParameterTypes(TestController.class);
     }
 
 
