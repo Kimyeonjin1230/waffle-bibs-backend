1804679197ca3910a100bdc1cacb20edc3458c15
Seung-9 <ssg9505fj22@naver.com>
2023-11-20 02:33:28 +0900

    chore: 잠시 sql 주석

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
-INSERT INTO category (title) SELECT 'Assignment' FROM dual
-WHERE NOT EXISTS (SELECT 1 FROM category WHERE title = 'Assignment');
-INSERT INTO category (title) SELECT 'work out' FROM dual
-WHERE NOT EXISTS (SELECT 1 FROM category WHERE title = 'work out');
-INSERT INTO category (title) SELECT 'daily' FROM dual
-WHERE NOT EXISTS (SELECT 1 FROM category WHERE title = 'daily');
-INSERT INTO category (title) SELECT 'meet' FROM dual
-WHERE NOT EXISTS (SELECT 1 FROM category WHERE title = 'meet');
+-- INSERT INTO category (title) SELECT 'Assignment' FROM dual
+-- WHERE NOT EXISTS (SELECT 1 FROM category WHERE title = 'Assignment');
+-- INSERT INTO category (title) SELECT 'work out' FROM dual
+-- WHERE NOT EXISTS (SELECT 1 FROM category WHERE title = 'work out');
+-- INSERT INTO category (title) SELECT 'daily' FROM dual
+-- WHERE NOT EXISTS (SELECT 1 FROM category WHERE title = 'daily');
+-- INSERT INTO category (title) SELECT 'meet' FROM dual
+-- WHERE NOT EXISTS (SELECT 1 FROM category WHERE title = 'meet');
