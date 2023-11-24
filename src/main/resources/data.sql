3027e7f914a1dbd1fed2fcdbb34bd82146e1918d
Seung-9 <ssg9505fj22@naver.com>
2023-11-24 10:24:56 +0900

    feat: 초기 테이블 생성 및 데이터 생성

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-insert into category (title) values ("assignment")
-insert into category (title) values ("work Out")
-insert into category (title) values ("daily")
-insert into category (title) values ("meet")
+INSERT INTO category (title) SELECT 'assignment' FROM dual WHERE NOT EXISTS (SELECT 1 FROM category WHERE title = 'assignment');
+INSERT INTO category (title) SELECT 'work out' FROM dual WHERE NOT EXISTS (SELECT 1 FROM category WHERE title = 'work out');
+INSERT INTO category (title) SELECT 'daily' FROM dual WHERE NOT EXISTS (SELECT 1 FROM category WHERE title = 'daily');
+INSERT INTO category (title) SELECT 'meet' FROM dual WHERE NOT EXISTS (SELECT 1 FROM category WHERE title = 'meet');
