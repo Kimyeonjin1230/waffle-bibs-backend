9fd68a8c889ed77c6b6e6f4ae9fbf5e6d5c715c2
Seung-9 <ssg9505fj22@naver.com>
2023-11-24 10:24:16 +0900

    chore: swagger ui 커스텀 및 url 변경

--- src/main/java/com/gdsc/waffle/controller/TodoController.java
+++ src/main/java/com/gdsc/waffle/controller/TodoController.java
@@ -2,6 +2,10 @@ package com.gdsc.waffle.controller;
 
 import com.gdsc.waffle.dto.TodoDto;
 import com.gdsc.waffle.service.TodoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,40 +16,46 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/")
+@Api(tags = "Todo 컨트롤러")
 public class TodoController {
 
     private final TodoService todoService;
 
-    // todo 목록 전체 가져오기
-    @GetMapping("/{categoryId}")
+    @ApiOperation(value = "todo 목록 전체 조회 API", notes = "카테고리 안에 들어있는 todo 목록 전체를 조회하는 API 입니다.")
+    @GetMapping("/{categoryId}/todo")
     public ResponseEntity<List<TodoDto>> getAllTodos(@PathVariable Long categoryId) {
         List<TodoDto> todos = todoService.findAll();
         return new ResponseEntity<>(todos, HttpStatus.OK);
     }
 
-    // todo 상세조회
-    @GetMapping("/{todoId}")
+    @ApiOperation(value = "todo 상세 조회 API", notes = "선택한 Todo의 자세한 정보를 조회하는 API 입니다.")
+    @ApiResponses({
+                    @ApiResponse(responseCode = "200", description = "성공적으로 조회되었습니다."),
+                    @ApiResponse(responseCode = "404", description = "해당 Todo를 찾을 수 없습니다.")
+    })
+    @GetMapping("/todo/{todoId}")
     public ResponseEntity<TodoDto> getTodo(@PathVariable Long todoId) {
+
         TodoDto todo = todoService.findById(todoId);
         return new ResponseEntity<>(todo, HttpStatus.OK);
     }
 
-    // todo 생성
-    @PostMapping("/{categoryId}/add")
+    @ApiOperation(value = "todo 생성 API", notes = "새로운 Todo를 생성하는 API 입니다.")
+    @PostMapping("/{categoryId}/todo/add")
     public ResponseEntity<TodoDto> addTodo(@PathVariable Long categoryId, @RequestBody TodoDto todoDto) {
         todoService.addTodo(categoryId, todoDto);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    // todo 수정
-    @PatchMapping("/{todoId}/update")
-    public ResponseEntity<TodoDto> updateTodo(@PathVariable Long todoId, @ModelAttribute TodoDto todoDto) {
+    @ApiOperation(value = "todo 수정 API", notes = "선택한 Todo의 내용을 수정하는 API 입니다.")
+    @PatchMapping("/todo/{todoId}/update")
+    public ResponseEntity<TodoDto> updateTodo(@PathVariable Long todoId, @RequestBody TodoDto todoDto) {
         todoService.update(todoId, todoDto);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    // todo 삭제
-    @DeleteMapping("/{todoId}")
+    @ApiOperation(value = "todo 삭제 API", notes = "선택한 Todo를 삭제하는 API 입니다.")
+    @DeleteMapping("/todo/{todoId}")
     public ResponseEntity<Void> deleteTodo(@PathVariable Long todoId) {
         todoService.deleteTodo(todoId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
